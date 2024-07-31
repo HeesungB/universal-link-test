5e44df5b97a8ee3ffc43059e2f08664df4d53dad
HeesungB <hezo25@naver.com>
2024-07-31 12:16:21 +0900

    add copy address test

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,17 @@ function App() {
   return (
     <div>
       <div className="card">
-        <button onClick={() => location.href = "https://deeplink.keplr.app/staking?chainId=akashnet-2&userIdentifier=abc&activityName=akash_stake"}>
-          Universal Link Test
+        <button
+          onClick={() => location.href = "https://deeplink.keplr.app/staking?chainId=akashnet-2&userIdentifier=gECb4ZvZXeLjg4tjW4-4Sa3G9RjklxQSFdfgDjSikBi5uR53SF2ghwLV4y9y7_0Wlcspj3r3&activityName=akash_stake"}>
+          Stake Link Test
+        </button>
+      </div>
+
+      <div className="card">
+
+        <button
+          onClick={() => location.href = "https://deeplink.keplr.app/show-address?chainId=akashnet-2"}>
+          Copy Address Link Test
         </button>
       </div>
     </div>
